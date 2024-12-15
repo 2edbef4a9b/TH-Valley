ed75f8fde76181c3afac69265a8c111c0d04d69f
Ran Li <echofedn@outlook.com>
2024-12-15 14:09:21 +0800

    feat(gui): Implement basic title screen (#2)
    * feat(gui): Add background image to title screen
    
    - Implement background image loading and scaling in the TitleScreen
    - Add title text to the top of TitleScreen scene
    - Create unit tests for TitleScreen scene
    
    * chore: Add title_screen_tests to CMakeLists.txt
    
    - Add the googletest target in the CMakeLists.txt file for TitleScene
      class for unit testing.
    - Copy the assets directory to the output directory after the build
      process for all tests. This makes sure that the tests can access the
      resources required without the need to build the main application.
    
    * refactor: Separate AddBackground func from init
    
    * feat(gui): Add button to title screen
    
    - Add a button to the middle of the title screen for testing purposes
    - The button's texture is controlled by both a mouse_listener and a
      TouchEventListener.
    - These listeners conflict because the mouse_listener is always called
      first and cannot be disabled.
    - Future commits will consider implementing a custom mouse_listener that
      can be disabled to prevent this conflict.
    
    * feat(gui): Add 4 buttons and title to title screen
    
    - Implement button hover effect by changing button's opacity, so that
      the button only needs one image
    - Separate button and button text so that the button text's opacity
      will not change when the button is hovered
    
    * feat(gui): Optimize title screen codes
    
    - Move constants to class members
    - Rename lambda function for better readability
    
    * feat(gui): Add callback for different buttons
    
    - Add callback as a parameter for the AddButton function to allow other
      game scenes and GUI elements to interact with the buttons on the title
      screen
    - Rename lambda functions to be more descriptive
    - Move the title text constant to TitleScreen class as static member for
      better encapsulation
    
    * chore: Add assets for title screen
    
    * chore: Update CMakeLists for linking resources
    
    - Create a function in CMakeLists.txt to copy the assets folder to the
      target directory and call it for every target.
    
    * refactor: Optimize code style
    
    - Add namespace to all classes of `Sample` module to avoid naming
      conflicts
    - Update resource paths setting logic for more flexibility and
      robustness
    - Move the resource paths setting function to `SampleApplication` class
      to make it more centralized
    - Change resource paths used in `SampleScene` and `TitleScreen` classes
      to adjust to the new setting logic
    - Format the code
    
    * chore: Modify CMakeLists.txt for correct function
    
    - Change `BUILD_TESTS` from `Option` to `Variable` in `CMakeLists.txt`
      to ensure that the tests building can be toggled on and off correctly
    - Modify `link_resources` function to create symbolic links to the
      resources in the output directory in Unix systems
    
    * feat: Support multiple bar colors in title screen

--- src/sample/sample_application.cpp
+++ src/sample/sample_application.cpp
@@ -3,19 +3,19 @@
 #include "AudioEngine.h"
 #include "sample/sample_scene.h"
 
-SampleApplication::~SampleApplication() {
+th_valley::SampleApplication::~SampleApplication() {
     // Release the shared instance of the audio engine.
     cocos2d::AudioEngine::end();
 }
 
-void SampleApplication::initGLContextAttrs() {
+void th_valley::SampleApplication::initGLContextAttrs() {
     // Set OpenGL context attributes:
     // red, green, blue, alpha, depth, stencil and multisamples count.
     GLContextAttrs gl_context_attrs{8, 8, 8, 8, 24, 8, 0};
     cocos2d::GLView::setGLContextAttrs(gl_context_attrs);
 }
 
-bool SampleApplication::applicationDidFinishLaunching() {
+bool th_valley::SampleApplication::applicationDidFinishLaunching() {
     // Initialize director.
     auto *director = cocos2d::Director::getInstance();
     auto *glview = director->getOpenGLView();
@@ -25,13 +25,21 @@ bool SampleApplication::applicationDidFinishLaunching() {
     }
 
     // Set the window size.
-    glview->setFrameSize(1280, 720);
+    glview->setFrameSize(kWindowSize.width, kWindowSize.height);
 
     // Set the design resolution size to improve resolution on high-res screens.
-    glview->setDesignResolutionSize(1920, 1080, ResolutionPolicy::NO_BORDER);
+    glview->setDesignResolutionSize(kDesignResolutionSize.width,
+                                    kDesignResolutionSize.height,
+                                    ResolutionPolicy::NO_BORDER);
 
     // Set FPS. the default value is 1.0/60 if you don't call this.
-    director->setAnimationInterval(1.0 / 60);
+    director->setAnimationInterval(kAnimationInterval);
+
+    // Remove the last directory name from the resource root path.
+    UpdateResourcePath();
+    const std::string resource_root_path =
+        cocos2d::FileUtils::getInstance()->getDefaultResourceRootPath();
+    CCLOG("Resource root path: %s", resource_root_path.c_str());
 
     // Create a scene. it's an autorelease object.
     auto *scene = SampleScene::create();
@@ -45,12 +53,42 @@ bool SampleApplication::applicationDidFinishLaunching() {
     return true;
 }
 
-void SampleApplication::applicationDidEnterBackground() {
+void th_valley::SampleApplication::applicationDidEnterBackground() {
+    cocos2d::Director::getInstance()->stopAnimation();
     // Pause the audio engine when the application enters the background.
     cocos2d::AudioEngine::pauseAll();
 }
 
-void SampleApplication::applicationWillEnterForeground() {
+void th_valley::SampleApplication::applicationWillEnterForeground() {
+    cocos2d::Director::getInstance()->startAnimation();
     // Resume the audio engine when the application enters the foreground.
     cocos2d::AudioEngine::resumeAll();
+}
+
+void th_valley::SampleApplication::UpdateResourcePath() {
+    static bool is_called = false;
+    // Ensure that this method is called only once.
+    if (is_called) {
+        return;
+    }
+    is_called = true;
+
+    // Set the resource path.
+    auto *file_utils = cocos2d::FileUtils::getInstance();
+
+    // Get the current resource root absolute path.
+    std::string resource_root_path =
+        cocos2d::FileUtils::getInstance()->getDefaultResourceRootPath();
+
+    // Remove the last directory name from the resource root path.
+    const size_t pos =
+        resource_root_path.find_last_of('/', resource_root_path.size() - 2);
+    if (pos != std::string::npos) {
+        resource_root_path = resource_root_path.substr(0, pos + 1);
+    } else {
+        resource_root_path = "/";
+    }
+
+    // Update the default resource root path.
+    file_utils->setDefaultResourceRootPath(resource_root_path);
 }
