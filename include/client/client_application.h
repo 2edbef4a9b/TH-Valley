85a1dcfd59f4c2dbf4aa7bad2c5e076a893d030f
2edbef4a9b <echofedn@outlook.com>
2024-12-17 19:58:59 +0800

    style: Optimize ClientApplication class
    - Delete unused headers for clean code
    - Remove member variable client_controller_ since it is now implemented
      using a singleton pattern
    - Delete some special member functions since ClientApplication class is
      implemented using a singleton pattern

--- include/client/client_application.h
+++ include/client/client_application.h
@@ -1,19 +1,19 @@
 #ifndef CLIENT_APPLICATION_H_
 #define CLIENT_APPLICATION_H_
 
-#include "client/client_controller.h"
 #include "cocos2d.h"
 
 namespace th_valley {
 
-class ClientApplication : private cocos2d::Application {
+class ClientApplication final : private cocos2d::Application {
 public:
     ClientApplication() = default;
     ~ClientApplication() override;
-    ClientApplication(const ClientApplication& other) = default;
-    ClientApplication& operator=(const ClientApplication& other) = default;
-    ClientApplication(ClientApplication&& other) = default;
-    ClientApplication& operator=(ClientApplication&& other) = default;
+
+    ClientApplication(const ClientApplication& other) = delete;
+    ClientApplication& operator=(const ClientApplication& other) = delete;
+    ClientApplication(ClientApplication&& other) = delete;
+    ClientApplication& operator=(ClientApplication&& other) = delete;
 
     bool applicationDidFinishLaunching() override;
     void applicationDidEnterBackground() override;
@@ -29,11 +29,9 @@ private:
         float height;
     };
 
-    static constexpr Size kWindowSize{1920, 1080};
-    static constexpr Size kDesignResolutionSize{1920, 1080};
+    static constexpr Size kWindowSize{.width = 1920, .height = 1080};
+    static constexpr Size kDesignResolutionSize{.width = 1920, .height = 1080};
     static constexpr float kInterval = 1.0F / 60;
-
-    std::shared_ptr<ClientController> client_controller_;
 };
 
 }  // namespace th_valley
