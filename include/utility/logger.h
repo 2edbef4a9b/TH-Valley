54dd88d3a8da7d8f71c15c466ce9c11b9fea65d6
2edbef4a9b <echofedn@outlook.com>
2024-12-18 20:56:18 +0800

    feat: Support std::format in Logger
    - Add support for std::format in Logger to allow for more flexible logging
      messages.

--- include/utility/logger.h
+++ include/utility/logger.h
@@ -1,6 +1,7 @@
 #ifndef LOGGER_H_
 #define LOGGER_H_
 
+#include <format>
 #include <string_view>
 
 namespace th_valley {
@@ -15,6 +16,21 @@ public:
     void LogError(std::string_view message);
     void LogFatal(std::string_view message);
 
+    template <typename... Args>
+    void LogDebug(std::string_view format, Args&&... args);
+
+    template <typename... Args>
+    void LogInfo(std::string_view format, Args&&... args);
+
+    template <typename... Args>
+    void LogWarning(std::string_view format, Args&&... args);
+
+    template <typename... Args>
+    void LogError(std::string_view format, Args&&... args);
+
+    template <typename... Args>
+    void LogFatal(std::string_view format, Args&&... args);
+
     // Delete member functions should be public.
     Logger(const Logger& other) = delete;
     Logger& operator=(const Logger& other) = delete;
@@ -27,6 +43,36 @@ private:
 
     void InitLogger();
 };
+
+template <typename... Args>
+void Logger::LogDebug(std::string_view format, Args&&... args) {
+    LogDebug(std::vformat(
+        format, std::make_format_args(std::forward<const Args>(args)...)));
+}
+
+template <typename... Args>
+void Logger::LogInfo(std::string_view format, Args&&... args) {
+    LogInfo(std::vformat(
+        format, std::make_format_args(std::forward<const Args>(args)...)));
+}
+
+template <typename... Args>
+void Logger::LogWarning(std::string_view format, Args&&... args) {
+    LogWarning(std::vformat(
+        format, std::make_format_args(std::forward<const Args>(args)...)));
+}
+
+template <typename... Args>
+void Logger::LogError(std::string_view format, Args&&... args) {
+    LogError(std::vformat(
+        format, std::make_format_args(std::forward<const Args>(args)...)));
+}
+
+template <typename... Args>
+void Logger::LogFatal(std::string_view format, Args&&... args) {
+    LogFatal(std::vformat(
+        format, std::make_format_args(std::forward<const Args>(args)...)));
+}
 
 }  // namespace th_valley
 
