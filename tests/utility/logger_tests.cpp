50cfe2fed7cc4d53aa2f9dd3602fb119ac8ce2f5
2edbef4a9b <echofedn@outlook.com>
2024-12-18 20:56:48 +0800

    test: Test the std::format supported by Logger

--- tests/utility/logger_tests.cpp
+++ tests/utility/logger_tests.cpp
@@ -2,13 +2,22 @@
 
 #include "utility/logger.h"
 
-TEST(LoggerTests, LogType) {
+TEST(LoggerTests, LogMessages) {
     // Test logging with different types.
     th_valley::Logger::GetInstance().LogDebug("Debug message");
     th_valley::Logger::GetInstance().LogInfo("Info message");
     th_valley::Logger::GetInstance().LogWarning("Warning message");
     th_valley::Logger::GetInstance().LogError("Error message");
     th_valley::Logger::GetInstance().LogFatal("Fatal message");
+}
+
+TEST(LoggerTests, LogFormatted) {
+    // Test formatted logging with different types.
+    th_valley::Logger::GetInstance().LogDebug("Debug message: {}", 42);
+    th_valley::Logger::GetInstance().LogInfo("Info message: {}", 3.14);
+    th_valley::Logger::GetInstance().LogWarning("Warning message: {}", "test");
+    th_valley::Logger::GetInstance().LogError("Error message: {}", true);
+    th_valley::Logger::GetInstance().LogFatal("Fatal message: {}", 'A');
 }
 
 int main(int argc, char** argv) {
