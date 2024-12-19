5945c8526596a5f55729df7d536664a34018e0d6
2edbef4a9b <echofedn@outlook.com>
2024-12-19 21:13:27 +0800

    Fix: Fix segfault in session_tests.cpp
    Move the server_.shutdown() call to the end of the test case to avoid
    segfaulting.

--- tests/server/session_tests.cpp
+++ tests/server/session_tests.cpp
@@ -1,6 +1,8 @@
+#include <client/client_application.h>
 #include <gtest/gtest.h>
 
 #include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
 #include <chrono>
 #include <thread>
 
@@ -11,7 +13,7 @@ namespace th_valley {
 
 class ServerClientTest : public ::testing::Test {
 protected:
-    ServerClientTest() : server_(server_io_context_, "28080") {}
+    ServerClientTest() : server_(server_io_context_, "65472") {}
 
     void SetUp() override {
         server_thread_ = std::thread([this]() {
@@ -19,17 +21,17 @@ protected:
             server_io_context_.run();
         });
 
-        // Give the server some time to start
         std::this_thread::sleep_for(std::chrono::seconds(1));
+
     }
 
     void TearDown() override {
-        // Shut down the server
-        server_.ShutDown();
         server_io_context_.stop();
         if (server_thread_.joinable()) {
             server_thread_.join();
         }
+
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 
     boost::asio::io_context server_io_context_;
@@ -40,17 +42,24 @@ protected:
 TEST_F(ServerClientTest, ClientServerCommunication) {
     NetworkClient& client = NetworkClient::GetInstance();
     client.SetUUID(boost::uuids::random_generator()());
-    client.Connect("127.0.0.1", "28080");
-
     // Set a callback to handle received messages
     client.SetCallback([](std::string_view message) {
-        std::cout << "Received message: " << message << std::endl;
+        std::cout << "Client Callback: " << message << std::endl;
     });
 
-    // Send a message to the server
-    client.SendMessages("Hello, Server!");
+    client.Connect("127.0.0.1", "65472");
 
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+
+    std::cout << "Client connected" << std::endl;
+
+    // Send the UUID to the server first
+    client.SendMessages(boost::uuids::to_string(client.GetUUID()));
+    client.SendMessages("Hello, server!");
+    client.SendMessages("Goodbye, server!");
     client.Disconnect();
+    server_.ShutDown();
 }
 
 }  // namespace th_valley
