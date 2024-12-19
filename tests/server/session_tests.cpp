999a25b814768a7aaa9cb26f50eef902544d7cf9
2edbef4a9b <echofedn@outlook.com>
2024-12-19 22:52:57 +0800

    test: Optimize the session tests
    - Remove unnecessary sleep calls
    - Handle the server's lifetime in the SetUp and TearDown methods for
      encapsulation and clean codes
    - Add some comments to describe the purpose of the tests

--- tests/server/session_tests.cpp
+++ tests/server/session_tests.cpp
@@ -8,6 +8,7 @@
 
 #include "client/network_client.h"
 #include "server/server.h"
+#include "utility/logger.h"
 
 namespace th_valley {
 
@@ -16,24 +17,23 @@ protected:
     ServerClientTest() : server_(server_io_context_, "65472") {}
 
     void SetUp() override {
+        // Start the server in server_thread_.
         server_thread_ = std::thread([this]() {
             server_.StartUp();
             server_io_context_.run();
         });
-
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-
     }
 
     void TearDown() override {
+        // Stop the server and join the server_thread_.
+        server_.ShutDown();
         server_io_context_.stop();
         if (server_thread_.joinable()) {
             server_thread_.join();
         }
-
-        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 
+private:
     boost::asio::io_context server_io_context_;
     Server server_;
     std::thread server_thread_;
@@ -41,25 +41,32 @@ protected:
 
 TEST_F(ServerClientTest, ClientServerCommunication) {
     NetworkClient& client = NetworkClient::GetInstance();
+
+    // Set the UUID for the client.
     client.SetUUID(boost::uuids::random_generator()());
-    // Set a callback to handle received messages
-    client.SetCallback([](std::string_view message) {
-        std::cout << "Client Callback: " << message << std::endl;
+
+    // Set a callback to handle received messages.
+    client.SetCallback([](std::string_view message) -> void {
+        Logger::GetInstance().LogInfo("Client Callback: Received: {}.",
+                                      message);
     });
 
+    // Connect the client to the server.
     client.Connect("127.0.0.1", "65472");
 
-    std::this_thread::sleep_for(std::chrono::seconds(3));
-
-
-    std::cout << "Client connected" << std::endl;
+    // Wait for the client to connect.
+    std::this_thread::sleep_for(std::chrono::seconds(1));
 
     // Send the UUID to the server first
     client.SendMessages(boost::uuids::to_string(client.GetUUID()));
     client.SendMessages("Hello, server!");
     client.SendMessages("Goodbye, server!");
+
+    // Wait for the server to process the messages.
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    // Disconnect the client from the server.
     client.Disconnect();
-    server_.ShutDown();
 }
 
 }  // namespace th_valley
