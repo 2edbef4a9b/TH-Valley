cc4ac656fd877fa5146fa5a012b5384f0f3259e6
2edbef4a9b <echofedn@outlook.com>
2024-12-19 20:54:54 +0800

    chore: Modify logging messages for consistency

--- src/client/network_client.cpp
+++ src/client/network_client.cpp
@@ -12,8 +12,8 @@ namespace th_valley {
 
 void NetworkClient::Connect(const std::string_view host,
                             const std::string_view port) {
-    Logger::GetInstance().LogInfo("Client: Connecting to server at {}:{}", host,
-                                  port);
+    Logger::GetInstance().LogInfo("Client: Connecting to server at {}:{}.",
+                                  host, port);
 
     // Resolve the host name into an IP address.
     const boost::asio::ip::tcp::resolver::query query(host.data(), port.data());
@@ -26,11 +26,11 @@ void NetworkClient::Connect(const std::string_view host,
                const boost::asio::ip::tcp::endpoint& endpoint) -> void {
             if (error_code) {
                 Logger::GetInstance().LogError(
-                    "Client: Connection failed with error: {}",
+                    "Client: Connection failed with error: {}.",
                     error_code.message());
             } else {
                 Logger::GetInstance().LogInfo(
-                    "Client: Connected to server at {}:{}",
+                    "Client: Connected to server at {}:{}.",
                     endpoint.address().to_string(), endpoint.port());
                 ReceiveMessages();
             }
@@ -39,11 +39,11 @@ void NetworkClient::Connect(const std::string_view host,
     // Run the io_context to start the asynchronous operations.
     io_context_thread_ = std::thread([this]() { io_context_.run(); });
 
-    Logger::GetInstance().LogInfo("Client: Connection established");
+    Logger::GetInstance().LogInfo("Client: Connection established.");
 }
 
 void NetworkClient::Disconnect() {
-    Logger::GetInstance().LogInfo("Client: Disconnecting from server");
+    Logger::GetInstance().LogInfo("Client: Disconnecting from server.");
 
     boost::system::error_code error_code;
     if (socket_.is_open()) {
@@ -52,7 +52,7 @@ void NetworkClient::Disconnect() {
                          error_code);
         if (error_code) {
             Logger::GetInstance().LogError(
-                "Client: Socket shutdown failed with error: {}",
+                "Client: Socket shutdown failed with error: {}.",
                 error_code.message());
         }
 
@@ -60,7 +60,7 @@ void NetworkClient::Disconnect() {
         socket_.close(error_code);
         if (error_code) {
             Logger::GetInstance().LogError(
-                "Client: Socket close failed with error: {}",
+                "Client: Socket close failed with error: {}.",
                 error_code.message());
         }
     } else {
@@ -73,56 +73,58 @@ void NetworkClient::Disconnect() {
         io_context_thread_.join();
     }
 
-    Logger::GetInstance().LogInfo("Client: Disconnected from server");
+    Logger::GetInstance().LogInfo("Client: Disconnected from server.");
 }
 
-void NetworkClient::SendMessages(std::string_view message_sv) {
-    Logger::GetInstance().LogInfo("Client: Sending: {}", message_sv);
-
-    std::string message = std::string(message_sv) + "\n";
+void NetworkClient::SendMessages(const std::string_view message_sv) {
+    std::string message(message_sv);
+    Logger::GetInstance().LogInfo("Client: Sending message: {} to server.",
+                                  message);
     boost::asio::async_write(
-        socket_, boost::asio::buffer(message, message.size()),
+        socket_, boost::asio::buffer(message + "\n", message.size()) + 1,
         [this, message](const boost::system::error_code& error_code,
                         std::size_t /*length*/) -> void {
             if (error_code) {
                 Logger::GetInstance().LogError(
-                    "Client: Send failed with error: {}", error_code.message());
+                    "Client: Send failed with error: {}.",
+                    error_code.message());
             } else {
-                Logger::GetInstance().LogInfo("Client: Sent: {}", message);
+                Logger::GetInstance().LogInfo(
+                    "Client: Sent message: {} to server.", message);
             }
         });
 
-    Logger::GetInstance().LogInfo("Client: Message sent initiated");
+    Logger::GetInstance().LogInfo("Client: Message sent initiated.");
 }
 
 void NetworkClient::ReceiveMessages() {
-    Logger::GetInstance().LogInfo("Client: Receiving messages");
+    Logger::GetInstance().LogInfo("Client: Receiving messages.");
     boost::asio::async_read_until(
         socket_, buffer_, '\n',
         [this](const boost::system::error_code& error_code,
                std::size_t /*length*/) -> void {
             if (error_code) {
                 Logger::GetInstance().LogError(
-                    "Client: Receive failed with error: {}",
+                    "Client: Receive failed with error: {}.",
                     error_code.message());
             } else {
                 std::istream input_stream(&buffer_);
                 std::string message;
                 std::getline(input_stream, message);
-                Logger::GetInstance().LogInfo("Client: Received: {}", message);
+                Logger::GetInstance().LogInfo("Client: Received: {}.", message);
                 callback_(message);
                 ReceiveMessages();
             }
         });
 
-    Logger::GetInstance().LogInfo("Client: Receive initiated");
+    Logger::GetInstance().LogInfo("Client: Receive initiated.");
 }
 
 void NetworkClient::SetCallback(
     std::function<void(std::string_view)> callback) {
-    Logger::GetInstance().LogInfo("Client: Setting callback");
+    Logger::GetInstance().LogInfo("Client: Setting callback.");
     callback_ = std::move(callback);
-    Logger::GetInstance().LogInfo("Client: Callback set");
+    Logger::GetInstance().LogInfo("Client: Callback set.");
 }
 
 void NetworkClient::SetUUID(boost::uuids::uuid uuid) { uuid_ = uuid; }
@@ -135,12 +137,12 @@ NetworkClient& NetworkClient::GetInstance() {
 }
 
 NetworkClient::NetworkClient() : socket_(io_context_), resolver_(io_context_) {
-    Logger::GetInstance().LogInfo("Client: NetworkClient constructor");
+    Logger::GetInstance().LogInfo("Client: NetworkClient constructor.");
 }
 
 NetworkClient::~NetworkClient() {
-    Logger::GetInstance().LogInfo("Client: NetworkClient destructor");
-    Logger::GetInstance().LogInfo("Client: NetworkClient destructed");
+    Logger::GetInstance().LogInfo("Client: NetworkClient destructor.");
+    Logger::GetInstance().LogInfo("Client: NetworkClient destructed.");
 }
 
 }  // namespace th_valley
