502fbb5084517a59438a457ab8450f18e22d8a19
2edbef4a9b <echofedn@outlook.com>
2024-12-20 20:39:58 +0800

    feat: Encapsulate logic in Server class
    - Handle multiple threads and accepting connections in the base class
      in the Server class to encapsulate the logic and make it easier to
      implement different types of servers as derived classes
    - Use mutex and atomic variables to handle thread synchronization in
      the Server class

--- include/server/server.h
+++ include/server/server.h
@@ -12,7 +12,7 @@ namespace th_valley {
 class Server {
 public:
     Server(boost::asio::io_context& io_context, std::string_view port);
-    ~Server();
+    virtual ~Server();
 
     Server(const Server& other) = delete;
     Server& operator=(const Server& other) = delete;
@@ -22,12 +22,20 @@ public:
     void StartUp();
     void ShutDown();
 
+protected:
+    bool IsRunning() const;
+    virtual void RunMainLoop();
+    static constexpr double kServerTickRate = 60.0;
+    static constexpr std::size_t kServerTickInterval =
+        static_cast<std::size_t>(1000.0 / kServerTickRate);
+
 private:
     void DoAccept() const;
-
     boost::asio::ip::tcp::acceptor acceptor_;
+    mutable std::mutex mutex_;
+    std::thread main_loop_thread_;
     std::shared_ptr<SessionManager> session_manager_;
-    bool is_running_{false};
+    std::atomic<bool> is_running_{false};
 };
 
 }  // namespace th_valley
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -13,25 +13,51 @@ Server::Server(boost::asio::io_context& io_context, const std::string_view port)
       session_manager_(std::make_shared<SessionManager>(acceptor_)) {}
 
 Server::~Server() {
+    std::lock_guard<std::mutex> lock(mutex_);
     if (is_running_) {
         ShutDown();
     }
 }
 
 void Server::StartUp() {
+    std::lock_guard<std::mutex> lock(mutex_);
     Logger::GetInstance().LogInfo("Server starting up.");
-    DoAccept();
     is_running_ = true;
+    main_loop_thread_ = std::thread([this]() -> void { RunMainLoop(); });
+    DoAccept();
     Logger::GetInstance().LogInfo("Server started up.");
 }
 
 void Server::ShutDown() {
+    std::lock_guard<std::mutex> lock(mutex_);
     Logger::GetInstance().LogInfo("Server shutting down.");
     acceptor_.close();
+    if (main_loop_thread_.joinable()) {
+        main_loop_thread_.join();
+    }
     is_running_ = false;
     Logger::GetInstance().LogInfo("Server shut down.");
 }
 
-void Server::DoAccept() const { session_manager_->StartAccept(); }
+bool Server::IsRunning() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return is_running_;
+}
+
+void Server::RunMainLoop() {
+    Logger::GetInstance().LogInfo("Server running main loop.");
+    while (is_running_) {
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(kServerTickInterval));
+    }
+    Logger::GetInstance().LogInfo("Server main loop ended.");
+}
+
+void Server::DoAccept() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    Logger::GetInstance().LogInfo("Server accepting connections.");
+    session_manager_->StartAccept();
+    Logger::GetInstance().LogInfo("Server accepted connections.");
+}
 
 }  // namespace th_valley
