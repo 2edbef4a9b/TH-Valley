68a06214fd1f27d8b81f0b35ef9d6bdce2f05a45
2edbef4a9b <echofedn@outlook.com>
2024-12-21 02:23:21 +0800

    chore: Delete unnecessary comments

--- src/sample/sample_scene.cpp
+++ src/sample/sample_scene.cpp
@@ -3,6 +3,8 @@
 #include <cstddef>
 #include <string>
 
+#include "game/tiled_map.h"
+
 namespace th_valley {
 
 bool SampleScene::init() {
@@ -11,27 +13,10 @@ bool SampleScene::init() {
         return false;
     }
 
-    // Create a label and add it to the scene.
-    /*constexpr size_t kFontSize = 120;
-    auto *label = cocos2d::Label::createWithTTF(
-        "Hello World!~", "assets/fonts/DFHannotateW5-A.ttf", kFontSize);
-    label->setPosition(cocos2d::Vec2(
-        cocos2d::Director::getInstance()->getVisibleSize().width / 2,
-        cocos2d::Director::getInstance()->getVisibleSize().height / 2));
-
-    this->addChild(label);*/
-
-    auto _map = Map::create("assets/maps/House.tmx");
+    auto _map = TiledMap::create("assets/maps/House.tmx");
     if (_map) {
         auto visibleSize = cocos2d::Director::getInstance()->getVisibleSize();
         auto mapSize = _map->getContentSize();
-
-        //// 设置地图的锚点为左下角
-        //_map->setAnchorPoint(cocos2d::Vec2(0, 0));
-
-        /*_map->setPosition(
-            cocos2d::Vec2((visibleSize.width - mapSize.width) / 2,
-                          (visibleSize.height - mapSize.height) / 2));*/
 
         _map->setVisible(true);
         this->addChild(_map);
