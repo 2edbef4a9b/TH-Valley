a2ed9b7e98be9bde3d2c90b2ea5dda947417dee0
2edbef4a9b <echofedn@outlook.com>
2024-12-21 10:51:01 +0800

    chore: Update map to the latest version

--- assets/maps/outdoor.tsx
+++ assets/maps/outdoor.tsx
@@ -1,254 +1,254 @@
-<? xml version = "1.0" encoding = "UTF-8" ?>
-    <tileset name="outdoor" tilewidth="16" tileheight="16" tilecount="2400" columns="30">
-        <image source="outdoor.png" width="480" height="1280" />
-        <tile id="112">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="113">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="114">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="117">
-            <properties>
-                <property name="Collidable" value="false" />
-            </properties>
-        </tile>
-        <tile id="118">
-            <properties>
-                <property name="Collidable" value="false" />
-            </properties>
-        </tile>
-        <tile id="119">
-            <properties>
-                <property name="Collidable" value="false" />
-            </properties>
-        </tile>
-        <tile id="142">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="143">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="144">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="146">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="147">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="148">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="179">
-            <properties>
-                <property name="Collidable" value="false" />
-            </properties>
-        </tile>
-        <tile id="181">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="182">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="184">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="191">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="198">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="199">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="213">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="217">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="218">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="220">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="228">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="229">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="241">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="276">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="277">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="301">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="302">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="306">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="320">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="321">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="349">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="488">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="489">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="490">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="491">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="521">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="551">
-            <properties>
-                <property name="Collidable" value="true" />
-            </properties>
-        </tile>
-        <tile id="591">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="592">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="593">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="700">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="704">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="730">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="732">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="734">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="760">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-        <tile id="764">
-            <properties>
-                <property name="Cuiltivable" value="true" />
-            </properties>
-        </tile>
-    </tileset>
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="outdoor" tilewidth="16" tileheight="16" tilecount="2400" columns="30">
+ <image source="outdoor.png" width="480" height="1280"/>
+ <tile id="112">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="Collidable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="Collidable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="119">
+  <properties>
+   <property name="Collidable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="142">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="143">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="144">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="Collidable" value="false"/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="191">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="198">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="199">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="213">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="217">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="218">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="220">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="228">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="241">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="276">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="277">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="301">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="302">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="306">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="320">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="321">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="349">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="488">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="489">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="490">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="491">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="521">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="551">
+  <properties>
+   <property name="Collidable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="591">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="592">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="593">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="700">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="704">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="730">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="732">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="734">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="760">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+ <tile id="764">
+  <properties>
+   <property name="Cuiltivable" value="true"/>
+  </properties>
+ </tile>
+</tileset>
