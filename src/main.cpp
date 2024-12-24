b30805c66df1afef8a57c0e00e77caa600cbf03b
2edbef4a9b <eden2352570@gmail.com>
2024-12-25 07:55:48 +0800

    chore: Add background image to README

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
+#include <iostream>
+
 #include "client/client_application.h"
 
 auto main(int /* argc */, char** /* argv */) -> int {
-    const th_valley::ClientApplication app;
-    return cocos2d::Application::getInstance()->run();
+    try {
+        const th_valley::ClientApplication app;
+        return cocos2d::Application::getInstance()->run();
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    } catch (...) {
+        std::cerr << "Unknown exception caught" << std::endl;
+        return EXIT_FAILURE;
+    }
 }
